e0bdc64eff2d2d2cf967a19ca4bb13c1a17fdcfa
MikhaelPedro <mikhaelpedro2000@gmail.com>
2025-02-12 22:16:28 -0300

    Add project files.

---
+++ Microondas/Controllers/Microondas.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Microondas.Controllers
+{
+    public class Microondas : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
